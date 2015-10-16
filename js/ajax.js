7111a9503d9d7847f9895da8c36b904085d36aba
James Newton <blackslate@lexogram.com>
2015-10-16 11:58:03 -0400

    Add doors.html
    - Doors may be locked
    - Click on a door to close all others and open the selected door
    - Click on an open door to zoom x 4.167

---
+++ js/ajax.js
@@ -0,0 +1,34 @@
+;(function (){
+  var $section = $("section")
+
+  $("a").click(getAjaxPage)
+
+  function getAjaxPage(event) {
+    var target = event.target
+    var result = /#(\w+)/.exec(target.href)
+    if (!result) {
+      // No hash: should we actually go to the URL?
+      return
+    }
+
+    result = "puzzles/" + result[1] + ".html"
+    console.log(result)
+
+    $section.load( result );
+
+    // $.ajax({
+    //   url: result
+    // , success: success
+    // , error: error
+    // , dataType: "html"
+    // })
+
+    // function error ( jqXHR, textStatus, errorThrown ) {
+    //   console.log(jqXHR, textStatus, errorThrown)
+    // }
+
+    // function success ( data, textStatus, jqXHR ) {
+    //   console.log(data)
+    // }
+  }
+})()
