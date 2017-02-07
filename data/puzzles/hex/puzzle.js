a115f781d22b74ffa450280bb09769347518e7f2
James Newton <github@lexogram.com>
2017-02-07 18:04:23 +0100

    Create 13 hex pieces
    - Not correctly placed
    - Last piece in each row of 4 is too narrow

--- data/puzzles/hex/puzzle.js
+++ data/puzzles/hex/puzzle.js
@@ -1,3 +1,5 @@
+"use strict"
+
 window.puzzle = {
   map: {}
 , hash: "test"
@@ -34,110 +36,163 @@ function getClientLoc(event) {
     // Code goes here
     var article = document.querySelector("article")
     var canvas = document.createElement("canvas")
+    var context = canvas.getContext("2d")
+
     var img = document.createElement("img")
     var sqrt3 = Math.sqrt(3)
     var width = sqrt3 * 2
     var hexLocs = [
-                                [8.0]
+                                [8, 0]
     , [8-sqrt3*3, 3], [8-sqrt3, 3], [8+sqrt3, 3], [8+sqrt3*3, 3]
     ,          [8-width, 6],    [8, 6],   [8+width, 6]
     , [8-sqrt3*3, 9], [8-sqrt3, 9], [8+sqrt3, 9], [8+sqrt3*3, 9]
     ,                           [8, 12]
     ]
     var hexPieces = []
-    var source
+    var imgSize
+      , unit
+      , hexWidth
+      , source
 
-        img.onload = function cookieCutter() {
-          var imgSize = img.width
-          var unit = imgSize / 16
-          var unitWidth = unit * width
+    img.onload = function cookieCutter() {
+      imgSize = img.width
+      unit = imgSize / 16
+      hexWidth = unit * width
 
-          canvas.width = imgSize
-          canvas.height = imgSize
+      ;(function createPolygonHole() {
+        canvas.width = imgSize
+        canvas.height = imgSize
 
+        context.globalAlpha = 0.5
+        context.drawImage(img, 0, 0)
+
+        context.globalCompositeOperation='destination-out';
+        context.fillStyle='#fff'
+        context.globalAlpha = 1
+
+        context.beginPath();
+        context.moveTo(unit * 8, 0)
+        context.lineTo(unit * 8 + hexWidth/2, unit)
+        context.lineTo(unit * 8 + hexWidth/2, unit * 3)
+
+        context.lineTo(unit * 8 + hexWidth, unit * 4)
+        context.lineTo(unit * 8 + hexWidth*1.5, unit * 3)
+        context.lineTo(unit * 8 + hexWidth*2, unit * 4)
+        context.lineTo(unit * 8 + hexWidth*2, unit * 6)
+        context.lineTo(unit * 8 + hexWidth*1.5, unit * 7)
+        context.lineTo(unit * 8 + hexWidth*1.5, unit * 9)
+        context.lineTo(unit * 8 + hexWidth*2, unit * 10)
+        context.lineTo(unit * 8 + hexWidth*2, unit * 12)
+        context.lineTo(unit * 8 + hexWidth*1.5, unit * 13)
+        context.lineTo(unit * 8 + hexWidth, unit * 12)
+        context.lineTo(unit * 8 + hexWidth/2, unit * 13)
+        context.lineTo(unit * 8 + hexWidth/2, unit * 15)
+        context.lineTo(unit * 8, unit * 16)
+
+        context.lineTo(unit * 8 - hexWidth/2, unit * 15)
+        context.lineTo(unit * 8 - hexWidth/2, unit * 13)
+        context.lineTo(unit * 8 - hexWidth, unit * 12)
+        context.lineTo(unit * 8 - hexWidth*1.5, unit * 13)
+        context.lineTo(unit * 8 - hexWidth*2, unit * 12)
+        context.lineTo(unit * 8 - hexWidth*2, unit * 10)
+        context.lineTo(unit * 8 - hexWidth*1.5, unit * 9)
+        context.lineTo(unit * 8 - hexWidth*1.5, unit * 7)
+        context.lineTo(unit * 8 - hexWidth*2, unit * 6)
+        context.lineTo(unit * 8 - hexWidth*2, unit * 4)
+        context.lineTo(unit * 8 - hexWidth*1.5, unit * 3)
+        context.lineTo(unit * 8 - hexWidth, unit * 4)
+
+        context.lineTo(unit * 8 - hexWidth/2, unit * 3)
+        context.lineTo(unit * 8 - hexWidth/2, unit * 1)
+        context.closePath()
+        context.fill()
+
+        // Set compositing back to its default
+        context.globalCompositeOperation='source-over';
+      })()
+
+      ;(function createHexes() {
+        var hexCount = hexLocs.length
+        var ii
+          , hexPoint
+
+        for (ii=0; ii < hexCount; ii+=1) {
+          createHex(hexLocs[ii])
+        }
+
+        function createHex(loc) {
+          var canvas = document.createElement("canvas")
           var context = canvas.getContext("2d")
+          var left = loc[0] * unit
+          var top = loc[1] * unit
+          var image
 
-          ;(function createPolygonHole() {
-            context.globalAlpha = 0.5
-            context.drawImage(img, 0, 0)
+          canvas.width = hexWidth
+          canvas.height = unit * 4
 
-            context.globalCompositeOperation='destination-out';
-            context.fillStyle='#fff'
-            context.globalAlpha = 1
+          context.beginPath()
+          context.moveTo(hexWidth / 2, 0)
+          context.lineTo(hexWidth, unit)
+          context.lineTo(hexWidth, unit * 3)
+          context.lineTo(hexWidth / 2, unit * 4)
+          context.lineTo(0, unit * 3)
+          context.lineTo(0, unit * 1)
+          context.closePath()
+          context.clip()
+          context.drawImage(
+            img
+          , left
+          , top
+          , hexWidth
+          , unit * 4
+          , 0
+          , 0
+          , hexWidth
+          , unit * 4
+          )
 
-            context.beginPath();
-            context.moveTo(unit * 8, 0)
-            context.lineTo(unit * 8 + unitWidth/2, unit)
-            context.lineTo(unit * 8 + unitWidth/2, unit * 3)
+          image = document.createElement("img")
+          image.src = canvas.toDataURL()
 
-            context.lineTo(unit * 8 + unitWidth, unit * 4)
-            context.lineTo(unit * 8 + unitWidth*1.5, unit * 3)
-            context.lineTo(unit * 8 + unitWidth*2, unit * 4)
-            context.lineTo(unit * 8 + unitWidth*2, unit * 6)
-            context.lineTo(unit * 8 + unitWidth*1.5, unit * 7)
-            context.lineTo(unit * 8 + unitWidth*1.5, unit * 9)
-            context.lineTo(unit * 8 + unitWidth*2, unit * 10)
-            context.lineTo(unit * 8 + unitWidth*2, unit * 12)
-            context.lineTo(unit * 8 + unitWidth*1.5, unit * 13)
-            context.lineTo(unit * 8 + unitWidth, unit * 12)
-            context.lineTo(unit * 8 + unitWidth/2, unit * 13)
-            context.lineTo(unit * 8 + unitWidth/2, unit * 15)
-            context.lineTo(unit * 8, unit * 16)
+          article.appendChild(image)
 
-            context.lineTo(unit * 8 - unitWidth/2, unit * 15)
-            context.lineTo(unit * 8 - unitWidth/2, unit * 13)
-            context.lineTo(unit * 8 - unitWidth, unit * 12)
-            context.lineTo(unit * 8 - unitWidth*1.5, unit * 13)
-            context.lineTo(unit * 8 - unitWidth*2, unit * 12)
-            context.lineTo(unit * 8 - unitWidth*2, unit * 10)
-            context.lineTo(unit * 8 - unitWidth*1.5, unit * 9)
-            context.lineTo(unit * 8 - unitWidth*1.5, unit * 7)
-            context.lineTo(unit * 8 - unitWidth*2, unit * 6)
-            context.lineTo(unit * 8 - unitWidth*2, unit * 4)
-            context.lineTo(unit * 8 - unitWidth*1.5, unit * 3)
-            context.lineTo(unit * 8 - unitWidth, unit * 4)
+          hexPieces.push(image)
+        }
 
-            context.lineTo(unit * 8 - unitWidth/2, unit * 3)
-            context.lineTo(unit * 8 - unitWidth/2, unit * 1)
-            context.closePath()
-            context.fill()
+      }())
 
-            // Set compositing back to its default
-            context.globalCompositeOperation='source-over';
-          })()
+      // var context = canvas.getContext("2d")
+      // context.beginPath()
+      // context.moveTo(hexWidth / 2, 0)
+      // context.lineTo(hexWidth, unit)
+      // context.lineTo(hexWidth, unit * 3)
+      // context.lineTo(hexWidth / 2, unit * 4)
+      // context.lineTo(0, unit * 3)
+      // context.lineTo(0, unit * 1)
+      // context.closePath()
+      // context.globalCompositeOperation = 'destination-out';
+      // context.clip()
+      // context.drawImage(img, -trim, -trim)
+      //
+    source = canvas.toDataURL()
 
-          // var context = canvas.getContext("2d")
-          // context.beginPath()
-          // context.moveTo(unitWidth / 2, 0)
-          // context.lineTo(unitWidth, unit)
-          // context.lineTo(unitWidth, unit * 3)
-          // context.lineTo(unitWidth / 2, unit * 4)
-          // context.lineTo(0, unit * 3)
-          // context.lineTo(0, unit * 1)
-          // context.closePath()
-          // context.globalCompositeOperation = 'destination-out';
-          // context.clip()
-          // context.drawImage(img, -trim, -trim)
-          //
-      source = canvas.toDataURL()
-
-      img.src = source
-      img.onload = null
-    }
+    img.src = source
+    img.onload = null
+  }
     //
     // img.onload = function cookieCutter() {
     //   var unit = img.width / 16
-    //   var unitWidth = unit * width
+    //   var hexWidth = unit * width
     //
     //   canvas.width = unit * width
     //   canvas.height = unit * 4
     //
     //   var context = canvas.getContext("2d")
     //   context.beginPath()
-    //   context.moveTo(unitWidth / 2, 0)
-    //   context.lineTo(unitWidth, unit)
-    //   context.lineTo(unitWidth, unit * 3)
-    //   context.lineTo(unitWidth / 2, unit * 4)
+    //   context.moveTo(hexWidth / 2, 0)
+    //   context.lineTo(hexWidth, unit)
+    //   context.lineTo(hexWidth, unit * 3)
+    //   context.lineTo(hexWidth / 2, unit * 4)
     //   context.lineTo(0, unit * 3)
     //   context.lineTo(0, unit * 1)
     //   context.closePath()
